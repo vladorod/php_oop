190709c94a2c8eb0a306bac7cce14680bf0ad6b3
Vladislav <34486493+vladorod@users.noreply.github.com>
2019-11-21 10:10:15 +0300

    class methods

--- 01/class/Car.php
+++ 01/class/Car.php
@@ -4,9 +4,19 @@
       public $color;
       public $wheels = 4;
       public $speed = 180;
+      public $year = 2002;
       var $brand; // анохронизм из 4 версии php тогда небыло public privated использовали var
 
       public $path = __DIR__ .'/test';
+
+      public function getCarInfo() {
+          echo "<h3>О моем авто:</h3>
+                Марка: {$this->brand} <br>
+                Цвет: {$this->color} <br>
+                Скорость: {$this->speed} <br>
+                Колеса: {$this->wheels} <br>
+                Год выпуска: {$this->year}";
+      }
 
   }
 
--- 01/index.php
+++ 01/index.php
@@ -11,17 +11,6 @@ function debug($data) {
 $o1 = new Car;
 $o2 = new Car;
 
-$o2->color = 'red';
-$o2->wheels = 8 ;
-$o2->speed = 200;
-$o2->year = 2002;
 
-
-
-echo "<h3>О моем авто:</h3>
-Марка: {$o2->brand} <br>
-Цвет: {$o2->color} <br>
-Скорость: {$o2->speed} <br>
-Колеса: {$o2->wheels} <br>
-Год выпуска: {$o2->year}
-";
+echo $o1->getCarInfo();
+echo $o2->getCarInfo();
