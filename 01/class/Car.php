7b314986ab46a47d337811a3bff83521cd0fff75
Vladislav <34486493+vladorod@users.noreply.github.com>
2019-11-21 10:00:14 +0300

    class and public varibles

--- 01/class/Car.php
+++ 01/class/Car.php
@@ -5,4 +5,8 @@
       public $wheels = 4;
       public $speed = 180;
       var $brand; // анохронизм из 4 версии php тогда небыло public privated использовали var
+
+      public $path = __DIR__ .'/test';
+
   }
+
--- 01/index.php
+++ 01/index.php
@@ -2,6 +2,8 @@
 require_once 'class/firstClass.php';
 require_once 'class/Car.php';
 
+error_reporting(-1); // вывод ошибок
+
 function debug($data) {
     echo `<pre>`. print_r($data) . `</pre>`;
 }
@@ -15,7 +17,11 @@ $o2->speed = 200;
 $o2->year = 2002;
 
 
-debug($o2);
-debug($o1);
 
-ehco
+echo "<h3>О моем авто:</h3>
+Марка: {$o2->brand} <br>
+Цвет: {$o2->color} <br>
+Скорость: {$o2->speed} <br>
+Колеса: {$o2->wheels} <br>
+Год выпуска: {$o2->year}
+";
